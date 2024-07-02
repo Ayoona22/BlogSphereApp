0e23f13ee2055871ef651901bc5c0081e1e12342
Ayoona22 <159051775+Ayoona22@users.noreply.github.com>
2024-07-02 15:30:09 +0530

    Update views.py

--- BloggerProject/BlogSphere/views.py
+++ BloggerProject/BlogSphere/views.py
@@ -13,3 +13,33 @@ def save_blog(request):
     blog = Blogs(title=title,content=content,author=author)
     blog.save()
     return redirect("write")
+def show_all(request):
+    blog=Blogs.objects.all()
+    context={
+        'blogs':blog
+    }
+    return render(request, 'allblogs.html',context=context)
+def get_one_blog(request,bid):
+    blog=Blogs.objects.get(pk=bid)
+    context={
+        'onlyblog':blog
+    }
+    return render(request,'singleblog.html',context=context)
+def delete_blog(request,bid):
+    blog=Blogs.objects.filter(pk=bid)
+    blog.delete()
+    return redirect("all")
+def updateblog(request,bid):
+    blog=Blogs.objects.get(pk=bid)
+    context={
+        'blogs':blog
+    }
+    print(context)
+    return render(request,'updateblog.html',context=context)
+def update_blog(request,bid):
+    blog=Blogs.objects.get(pk=bid)
+    blog.title = request.POST['title']
+    blog.content = request.POST['content']
+    blog.author = request.POST['author']
+    blog.save()
+    return redirect("all")
