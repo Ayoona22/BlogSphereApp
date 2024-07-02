a8c23e5d0df5861f6917fbc8302c65dcc60d89b5
Ayoona22 <159051775+Ayoona22@users.noreply.github.com>
2024-07-02 14:12:25 +0530

    Update views.py

--- BloggerProject/BlogSphere/views.py
+++ BloggerProject/BlogSphere/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def get_homepage(request):
     return render(request, 'homepage.html',context={})
+def get_write(request):
+    return render(request, 'writepage.html',context={})
+def save_blog(request):
+    title = request.POST['title']
+    content = request.POST['content']
+    author = request.POST['author']
+    print("SAVED")
+    blog = Blogs(title=title,content=content,author=author)
+    blog.save()
+    return redirect("write")
