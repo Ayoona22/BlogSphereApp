f7e032ad4bf8305fd4be909bf8188e0135d6a8ed
Ayoona22 <159051775+Ayoona22@users.noreply.github.com>
2024-07-02 15:28:45 +0530

    Update urls.py

--- BloggerProject/BlogSphere/urls.py
+++ BloggerProject/BlogSphere/urls.py
@@ -5,4 +5,9 @@ urlpatterns =[
     path('homepage',views.get_homepage,name='homepage'),
     path('write',views.get_write,name='write'),
     path('save',views.save_blog,name='save'),
+    path('all',views.show_all,name='all'),
+    path('<int:bid>/',views.get_one_blog,name='oneblog'),
+    path('delete/<int:bid>/',views.delete_blog,name='delblog'),
+    path('update/<int:bid>/',views.updateblog,name='updateblog'),
+    path('update_/<int:bid>/',views.update_blog,name='update_blog'),
 ]
