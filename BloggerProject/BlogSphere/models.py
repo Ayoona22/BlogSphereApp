770086166757cb8ac6bf6677cb9ca4e3bd162b66
Ayoona22 <159051775+Ayoona22@users.noreply.github.com>
2024-07-02 12:28:15 +0530

    models.py

--- BloggerProject/BlogSphere/models.py
+++ BloggerProject/BlogSphere/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+class Blogs(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    author = models.CharField(max_length=20)
+
